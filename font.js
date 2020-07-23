dc3b453e73880dffa9ec78b69c597ba36e5728b5
ByungWoo <63662808+bw-99@users.noreply.github.com>
2020-07-23 13:44:54 +0900

    Create font.js

---
+++ font.js
@@ -0,0 +1,7 @@
+
+<script src="https://use.fontawesome.com/c21718356f.js"></script>
+
+
+<i aria-hidden="true" class="fas fa-car" title="Time to destination by car"></i>
+<span class="sr-only">Time to destination by car:</span>
+<span>4 minutes</span>
